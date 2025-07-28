6d24bd2c61659fd3f4494592ce58cbd25ed551bb
slvignesh05 <123097688+slvignesh05@users.noreply.github.com>
2025-07-28 23:45:27 +0530

    Update BuildRCE.swift

--- Plugins/BuildRCE/BuildRCE.swift
+++ Plugins/BuildRCE/BuildRCE.swift
@@ -1,16 +1,4 @@
-import PackagePlugin
+import Foundation
 
-@main
-struct BuildRCE: BuildToolPlugin {
-    func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
-        let tool = try context.tool(named: "BuildRCE")
-        return [
-            .buildCommand(
-                displayName: "Executing BuildRCE Plugin",
-                executable: tool.path,
-                arguments: [],
-                outputFilesDirectory: context.pluginWorkDirectory
-            )
-        ]
-    }
-}
+print("rce by w3shi")
+
