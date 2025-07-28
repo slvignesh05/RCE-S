21de16e28525858f586d5cb010157ea714379eff
slvignesh05 <123097688+slvignesh05@users.noreply.github.com>
2025-07-28 23:55:19 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,29 +2,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "RCE-S",
-    products: [
-        .library(name: "Jinja", targets: ["Jinja"])
-    ],
+    name: "rce-s",
     targets: [
-        // Helper tool the plugin will run (prints "rce")
         .executableTarget(
-            name: "EchoRCE",
-            dependencies: []
+            name: "EchoRCE"
         ),
-        // Your normal target (anything minimal is fine)
-        .target(
-            name: "Jinja",
-            dependencies: [],
-            plugins: [
-                .plugin(name: "BuildRCE")
-            ]
-        ),
-        // Build tool plugin that invokes EchoRCE during build
         .plugin(
             name: "BuildRCE",
             capability: .buildTool(),
-            path: "Plugins/BuildRCE"
+            dependencies: ["EchoRCE"]
         )
     ]
 )
