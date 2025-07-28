76f8ea8f849a019055c27a139dd3babc5f6c2d9f
slvignesh05 <123097688+slvignesh05@users.noreply.github.com>
2025-07-28 23:49:11 +0530

    Update plugin.swift

--- Plugins/BuildRCE/plugin.swift
+++ Plugins/BuildRCE/plugin.swift
@@ -3,14 +3,12 @@ import PackagePlugin
 @main
 struct BuildRCE: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
-        let script = context.pluginWorkDirectory.appending("echo_rce.sh")
+        let script = context.pluginWorkDirectory.appending("echo_rce.bat")
 
         try """
-        #!/bin/bash
+        @echo off
         echo rce
         """.write(to: URL(fileURLWithPath: script.string), atomically: true, encoding: .utf8)
-
-        try FileManager.default.setAttributes([.posixPermissions: 0o755], ofItemAtPath: script.string)
 
         return [
             .buildCommand(
@@ -22,3 +20,4 @@ struct BuildRCE: BuildToolPlugin {
         ]
     }
 }
+
